c7e4fcecaca1394345881c2725a533a53cb635f7
Yael <yaelsantiago1182@gmail.com>
2025-12-10 14:26:22 -0600

    chore: protect all routes and add gitignore for heavy files

--- src/router.js
+++ src/router.js
@@ -16,16 +16,16 @@ import ExplorarMundo from './views/ExplorarMundo.vue'
 import ConoceTuPais from './views/ConoceTuPais.vue'
 
 const routes = [
-  { path: '/', component: Home, name: 'Home' },
+  { path: '/', component: Home, name: 'Home', meta: { requiresAuth: true } },
   { path: '/login', component: Login, name: 'Login', meta: { requiresGuest: true } },
   { path: '/register', component: Register, name: 'Register', meta: { requiresGuest: true } },
   { path: '/profile', component: Profile, name: 'Profile', meta: { requiresAuth: true } },
-  { path: '/destinations', component: Destinations, name: 'Destinations' },
-  { path: '/destinations/:id', component: DestinationDetail, name: 'DestinationDetail' },
-  { path: '/wallet', component: Wallet, name: 'Wallet' },
-  { path: '/visitas-raras', component: VisitasRaras, name: 'VisitasRaras' },
-  { path: '/explorar-mundo', component: ExplorarMundo, name: 'ExplorarMundo' },
-  { path: '/conoce-tu-pais', component: ConoceTuPais, name: 'ConoceTuPais' },
+  { path: '/destinations', component: Destinations, name: 'Destinations', meta: { requiresAuth: true } },
+  { path: '/destinations/:id', component: DestinationDetail, name: 'DestinationDetail', meta: { requiresAuth: true } },
+  { path: '/wallet', component: Wallet, name: 'Wallet', meta: { requiresAuth: true } },
+  { path: '/visitas-raras', component: VisitasRaras, name: 'VisitasRaras', meta: { requiresAuth: true } },
+  { path: '/explorar-mundo', component: ExplorarMundo, name: 'ExplorarMundo', meta: { requiresAuth: true } },
+  { path: '/conoce-tu-pais', component: ConoceTuPais, name: 'ConoceTuPais', meta: { requiresAuth: true } },
   { path: '/reservations', component: Reservations, name: 'Reservations', meta: { requiresAuth: true } },
   { path: '/reservations/:id', component: ReservationDetail, name: 'ReservationDetail', meta: { requiresAuth: true } }
 ]
